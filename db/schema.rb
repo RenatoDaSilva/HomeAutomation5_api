6a8c6cca464e4a36506c7aa5ee58fd3d07b3f662
RenatoDaSilva <renato@asseinfo.com.br>
2019-08-14 22:01:54 -0300

    add devices storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_025227) do
+ActiveRecord::Schema.define(version: 2019_08_14_025756) do
+
+  create_table "devices", force: :cascade do |t|
+    t.string "name"
+    t.integer "pin"
+    t.boolean "state"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_devices_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 2019_08_14_025227) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "devices", "users"
 end
