889c15328fba23e15a8344ac505bf42507afea29
Renato da Silva <renato@asseinfo.com.br>
2019-09-03 00:23:42 -0300

    Adds repeat to schedule #10 (#11)
    resolve #10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_014101) do
+ActiveRecord::Schema.define(version: 2019_09_03_021137) do
 
   create_table "schedules", force: :cascade do |t|
     t.integer "pin"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_08_30_014101) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rate"
+    t.integer "interval"
     t.index ["user_id"], name: "index_schedules_on_user_id"
   end
 
