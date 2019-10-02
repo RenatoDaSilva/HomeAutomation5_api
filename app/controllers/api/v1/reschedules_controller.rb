233108c67b1da2c921fe9fdc050582ba0c3b69f7
RenatoDaSilva <renato@asseinfo.com.br>
2019-10-02 00:20:39 -0300

    returns next scheduled appointment when reschedules the current one

--- app/controllers/api/v1/reschedules_controller.rb
+++ app/controllers/api/v1/reschedules_controller.rb
@@ -5,18 +5,47 @@ class Api::V1::ReschedulesController < Api::V1::ApiController
  
   before_action :set_reschedule, only: [:show]
   before_action :require_authorization!, only: [:show]
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
   
-    # GET /api/v1/schedules
-    def index
-      @schedules = current_user.schedules
-      render json: @schedules.where("fire_on <= ?", DateTime.current.in_time_zone('Brasilia'))
-    end
+  # GET /api/v1/reschedules
+  def index
+    @schedules = current_user.schedules
+    render json: @schedules.where("fire_on <= ?", DateTime.current.in_time_zone('Brasilia')).order("fire_on")
+  end
     
   # GET /api/v1/reschedules/1
   def show
     if @schedule.rate.blank?
       @schedule.destroy
-   else
+    else
+      do_reschedule
+    end
+
+    bring_next_schedule
+  end
+  
+  private
+ 
+  def set_reschedule
+    @schedule = Schedule.find(params[:id])||nil
+  end
+  
+  def require_authorization!
+    unless current_user == @schedule.user
+      render json: {}, status: :forbidden
+    end
+  end
+
+  def handle_record_not_found
+    bring_next_schedule
+  end
+    
+  def bring_next_schedule
+    @schedules = current_user.schedules
+    render json: @schedules.where("fire_on <= ?", DateTime.current.in_time_zone('Brasilia')).order("fire_on").first
+  end
+    
+  def do_reschedule
     case @schedule.interval
     when 1
       @schedule.fire_on += @schedule.rate.minutes
@@ -26,24 +55,9 @@ class Api::V1::ReschedulesController < Api::V1::ApiController
       @schedule.fire_on += @schedule.rate.days
     end
 
-    if @schedule.save
-       render json: @schedule
-      else
-        render json: @schedule.errors, status: :unprocessable_entity
-      end
-   end
- end
-  
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reschedule
-      @schedule = Schedule.find(params[:id])
+    if !@schedule.save
+      render json: @schedule.errors, status: :unprocessable_entity
     end
-  
-    def require_authorization!
-      unless current_user == @schedule.user
-        render json: {}, status: :forbidden
-      end
-    end
+  end
 end
  
